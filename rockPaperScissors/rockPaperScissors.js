59c24d90d2d01252219d1a0243b536830f986323
Razvan Rotundu <raz.rotundu@gmail.com>
2024-06-12 14:34:04 -0400

    rockPaperScissors
    add template html to run in browser

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -1,5 +1,36 @@
 
+// Get Ai move
+function getAiMove(){
+    let num = Math.random();
+    if ( num < 0.33){
+        return "rock";
+    }
+    else if( num > 0.66 ){
+        return "scissor";
+    }
+    else{
+        return "paper"
+    }
+}
+// Get player move
+function getPlayerMove(){
+    do{
+        let move = prompt("Player move: ", "nothing");
+        move = move.toLowerCase();
+    }
+    while(move != "rock" || move != "paper" || move != "scissors");
+    return move
+
+}
+// 
 function main(){
-    console.log("Hello World");
+    let playerScore = 0;
+    let aiScore = 0;
+
+    // Play 5 rounds
+    for(let i = 0; i < 5; i++){
+        console.log(getPlayerMove());
+    }
+;
 }
 main()
