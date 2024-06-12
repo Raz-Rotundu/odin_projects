95b571b78b4c2b174597aa68f2a025d5282699f1
Razvan Rotundu <raz.rotundu@gmail.com>
2024-06-12 15:02:36 -0400

    rock paper scissors
    implement game through alerts

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -1,4 +1,20 @@
 
+// Determine winner, return 1 if player won, 2 if ai won
+function getWinner(playerMove, aiMove){
+    if(playerMove === "rock" && aiMove === "scissor") {
+        return 1;
+    }
+    else if(playerMove === "paper" && aiMove === "rock") {
+        return 1;
+    }
+    else if(playerMove === "scissor" && aiMove === "paper") {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
 // Get Ai move
 function getAiMove(){
     let num = Math.random();
@@ -12,25 +28,42 @@ function getAiMove(){
         return "paper"
     }
 }
+
 // Get player move
 function getPlayerMove(){
+    let move = "";
     do{
-        let move = prompt("Player move: ", "nothing");
+        move = prompt("Player move: ", "nothing");
         move = move.toLowerCase();
     }
-    while(move != "rock" || move != "paper" || move != "scissors");
+    while(move != "rock" && move != "paper" && move != "scissors");
     return move
 
 }
-// 
+
 function main(){
     let playerScore = 0;
     let aiScore = 0;
 
     // Play 5 rounds
     for(let i = 0; i < 5; i++){
-        console.log(getPlayerMove());
+        let playerMove = getPlayerMove();
+        let aiMove = getAiMove();
+        winner = getWinner(playerMove, aiMove);
+        if(winner == 1){
+            playerScore ++;
+            alert("Player has won")
+        }
+        else{
+            aiScore ++;
+            alert("Ai has won")
+        }
     }
-;
+    if(playerScore > aiScore){
+        alert("Player wins the game")
+    }
+    else{
+        alert("Ai wins the game")
+    }
 }
 main()
