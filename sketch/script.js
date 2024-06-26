a491359c51fb2bfe83c67272fada4969600e02b0
Razvan Rotundu <raz.rotundu@gmail.com>
2024-06-26 13:55:30 -0400

    fill out basics on html

---
+++ sketch/script.js
@@ -0,0 +1,21 @@
+// Makes a button DOM object
+function makeButton(){
+    return 1;
+}
+
+// Adds the object to a specified div class
+function addButton(div){
+    return 1;
+}
+
+// test
+function helloWorld(){
+    console.log("hello world");
+}
+
+
+// Main function
+document.addEventListener("DOMContentLoaded", () =>{
+    console.log("Page is loaded");
+    helloWorld();
+})
